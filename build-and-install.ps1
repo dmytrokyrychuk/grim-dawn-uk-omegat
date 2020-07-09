866f386049fe9773e34457c08c7470bc6a5e9f13
Dmytro Kyrychuk <dmytro@kyrych.uk>
2020-07-09 18:44:56 +0300

    Change build script paths

--- build-and-install.ps1
+++ build-and-install.ps1
@@ -1,2 +1,4 @@
-& 'C:\Program Files\7-Zip\7z' a -tzip uk.zip .\target\*
-mv -Force uk.zip 'C:\Program Files (x86)\Steam\steamapps\common\Grim Dawn\localization'
+$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+& 'C:\Program Files\7-Zip\7z' a -tzip $scriptDir\uk.zip $scriptDir\target\*
+mv -Force $scriptDir\uk.zip 'C:\Program Files (x86)\Steam\steamapps\common\Grim Dawn\localization'
