fde752111adc36d5e2d5d9e6d689c6922ceb4d2a
Dmytro Kyrychuk <dmytro@kyrych.uk>
2020-07-12 11:39:45 +0300

    Move language.def back to source

--- build-and-install.ps1
+++ build-and-install.ps1
@@ -1,4 +1,4 @@
 $scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-& 'C:\Program Files\7-Zip\7z' a -tzip $scriptDir\uk.zip $scriptDir\target\* $scriptDir\extra\*
+& 'C:\Program Files\7-Zip\7z' a -tzip $scriptDir\uk.zip $scriptDir\target\* $scriptDir\source\language.def
 mv -Force $scriptDir\uk.zip 'C:\Program Files (x86)\Steam\steamapps\common\Grim Dawn\localization'
